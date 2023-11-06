32c300e1c640a6e7a10e1c4bf4a4f74c6c91177e
SN5ON <shalomnyalila@gmail.com>
2023-11-06 15:25:15 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    """returns a tuple with the length of a string and its first character"""
     length = len(sentence)
     char = sentence[0] if length > 0 else "None"
     tup = length, char
