2139a8dfb33a58942660d92337c2dd48db5204e5
SN5ON <shalomnyalila@gmail.com>
2023-11-21 14:00:45 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class sqaure from with parameter"""
+
+
+class Square:
+    """class sqaure with parameter"""
+
+    def __init__(self, size):
+        """Initialize of a function with parameter size"""
+
+        self.__size = size
+
