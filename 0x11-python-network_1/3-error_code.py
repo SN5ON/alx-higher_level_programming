525850f53ec6b038c90694b61d7c21230ae583d3
SN5ON <shalomnyalila@gmail.com>
2024-04-26 18:04:25 -0700

    corrections

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Send request o the URL and displays the body"""
-import urllib.request
-import urllib.error
+"""takes in a URL, sends a request to
+the URL and displays the body of the response
+"""
 import sys
+import urllib.error
+import urllib.request
 
-
-def sender():
-    """sender"""
-    try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            html = response.read()
-            print(html.decode("utf-8"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
 
 if __name__ == "__main__":
-    sender()
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
